88812657cabaf0564f710ad045eda6b29f85fd9c
Fabian Scheid <fabian@nuwork.de>
2024-02-23 21:08:48 +0100

    renamed plugin files

--- lua/func_jumpr.lua
+++ lua/func_jumpr.lua
@@ -1,5 +1,5 @@
 -- main module file
-local module = require("plugin_name.module")
+local module = require("func_jumpr.module")
 
 ---@class Config
 ---@field opt string Your config option
---
+++ plugin/func_jumpr.lua
@@ -0,0 +1,1 @@
+vim.api.nvim_create_user_command("MyFirstFunction", require("func_jumpr").hello, {})
