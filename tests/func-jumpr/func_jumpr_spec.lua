9edc12d896fb74f2bab1e5dd545bb34b8777e95e
Beargruug <fabian.scheid@pocket-rocket.io>
2024-10-25 12:30:26 +0200

    update todo

--- tests/func-jumpr/func_jumpr_spec.lua
+++ tests/func-jumpr/func_jumpr_spec.lua
@@ -1,12 +1,1 @@
-local plugin = require("func_jumpr")
-
-describe("setup", function()
-  it("works with default", function()
-    assert(plugin.show_function() == "Hello!", "my first function with param = Hello!")
-  end)
-
-  it("works with custom var", function()
-    plugin.setup({ opt = "custom" })
-    assert(plugin.get_functions() == "custom", "my first function with param = custom")
-  end)
-end)
+--TODO: add tests
