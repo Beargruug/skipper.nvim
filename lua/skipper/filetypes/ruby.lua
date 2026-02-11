ef6bbcbbfccb2a2bc65cabc17653ebfb3383da61
Beargruug <info@fabianmail.com>
2026-02-11 09:06:03 +0100

    fix: ruby module structure and always use recursion

--- lua/skipper/filetypes/ruby.lua
+++ lua/skipper/filetypes/ruby.lua
@@ -1,17 +1,8 @@
 local M = {}
 
--- Node types that contain methods we want to extract
-local CONTAINER_TYPES = {
-    module = true,
-    class = true,
-    singleton_class = true, -- class << self
-    root = true, -- top-level
-}
-
--- Node types that are methods
 local METHOD_TYPES = {
     method = true,
-    singleton_method = true, -- def self.foo
+    singleton_method = true,
 }
 
 local function extract_methods(node, functions)
@@ -20,30 +11,17 @@ local function extract_methods(node, functions)
     end
     local node_type = node:type()
 
-    -- If this is a method, extract it
     if METHOD_TYPES[node_type] then
         local name_node = node:field("name")[1]
         if name_node then
-            local func_name = vim.treesitter.get_node_text(node, 0)
+            local func_name = vim.treesitter.get_node_text(name_node, 0)
             local start_row = vim.treesitter.get_node_range(node)
             table.insert(functions, { name = func_name, line = start_row })
         end
     end
 
-    -- If this is a container, recurse into its body
-    if CONTAINER_TYPES[node_type] then
-        local body = node:field("body")[1]
-        if body then
-            for _, child in ipairs(body:named_children()) do
-                extract_methods(child, functions)
-            end
-        end
-    end
-
-    if node_type == "root" then
-        for _, child in ipairs(node:named_children()) do
-            extract_methods(child, functions)
-        end
+    for child in node:iter_children() do
+        extract_methods(child, functions)
     end
 end
 
